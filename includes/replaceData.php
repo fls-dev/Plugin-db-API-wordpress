f9a39f762d6034351755d220d33f1e54bfa0db25
Pavel Misko <3764426@mail.ru>
2023-01-06 00:14:28 +0100

    init version

--- includes/replaceData.php
+++
@@ -1,8 +0,0 @@
-<?php
-$jsonString = $_POST['feedback'];
-echo $jsonString;
-$newJsonString = json_encode($jsonString);
-//unlink('feedback.json');
-file_put_contents("feedback.json", $newJsonString);
-$ok = 'ok';
-echo $ok;
